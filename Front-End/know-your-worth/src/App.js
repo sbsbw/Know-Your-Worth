b6418b0a8e1b0cb68860c67aa2d6b48be2f412bb
Devun Durst <devunfoxdurst@yahoo.com>
2024-01-29 10:49:48 -0700

    Hello, It is push monday! I implement a start to the webpage, a search bar feature in react libraries. This is just the start to many more components out there.

---
+++ Front-End/know-your-worth/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Searchbar from './components/Searchbar';
+
+const App = () => {
+  return (
+    <div>
+      <Searchbar />
+    </div>
+  );
+};
+
+export default App;
