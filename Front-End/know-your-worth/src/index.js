33687537ab20775898a1d32746c44bdd34659be8
Devun Durst <devunfoxdurst@yahoo.com>
2024-01-30 10:54:17 -0700

    filtered down to 1 react file, seemed to duplicate it on accident

--- Front-End/know-your-worth/src/index.js
+++
@@ -1,9 +0,0 @@
-// the starting point the first code that runs
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import './index.css';
-import App from './App';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-// html in javascript is JSX code and converted behind the scenes
