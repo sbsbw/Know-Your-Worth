0a0b617d80da89738af0262d4cbccdab362d9f5a
Sean Walker <Sbsbw@byui.edu>
2024-04-03 12:14:16 -0600

    I styled the about-us page.

--- client/src/pages/App-about-us.jsx
+++ client/src/pages/App-about-us.jsx
@@ -1,8 +1,11 @@
 import "../styles/App.css";
 import "../styles/App1.css";
-import img6 from "../images/facebook-logo.webp"
-import img7 from "../images/x-logo.webp"
-import img8 from "../images/instagram-logo.webp"
+import img9 from "../images/young-professionals.webp"
+import img10 from "../images/Logan-prof-photo.webp"
+import img11 from "../images/Devun-prof-photo.webp"
+import img12 from "../images/Lincoln-prof-photo.webp"
+import img13 from "../images/Hunter-prof-photo.webp"
+import img14 from "../images/Sean-prof-photo.webp"
 
 export default function AppAboutUs() {
   return (
@@ -10,32 +13,25 @@ export default function AppAboutUs() {
       {/* body open */}
       <div>
         {/* main close */}
-        <div className="container">
-          <h1>About Us</h1>
-          <div className="divider"></div>
+        <h1>About Us</h1>
+        <div className="divider"></div>
+        <div className="job-container">
           <picture>
-            <source
-              media="(max-width: 800px)"
-              srcset="./images/young-professionals-sm.webp"
-            />
-            <source
-              media="(max-width: 1200px)"
-              srcset="./images/young-professionals-md.webp"
-            />
             <img
-              src="./images/young-professionals.webp"
-              alt="young-professionals"
+              src={img9}
+              alt="Job Interview Image"
               loading="lazy"
             />
           </picture>
           <p>
-            Know Your Worth is a job search platform that connects job seekers
-            with job providers. We are dedicated to helping individuals find
-            their dream job.
+            Know Your Worth is a job search platform that informs job seekers
+            of the pay range they deserve so that they can make resonable
+            requests for compensation or know what jobs are not paying well
+            enough.
           </p>
         </div>
         <div className="divider"></div>
-        <div className="container">
+        <div className="job-container">
           <h1>Our Mission</h1>
           <p>
             Welcome to Know Your Worth, your trusted companion in the journey to
@@ -45,34 +41,14 @@ export default function AppAboutUs() {
           </p>
           <p>
             Who are we? We are more than just a website; we are a team of
-            passionate individuals driven by the mission to empower individuals
-            like you in their career pursuits. Our dedicated team works
-            tirelessly to curate a seamless platform that connects job seekers
-            with job providers effortlessly.
+            passionate individuals driven by the mission to assist anyone
+            to know how much they are worth and develop the skills they need
+            to find the right job for them. 
           </p>
           <p>
-            What sets us apart? We pride ourselves on offering a user-friendly
-            interface designed to simplify the job search process. Whether
-            you're a seasoned professional looking for a career change or a
-            fresh graduate stepping into the workforce, Know Your Worth caters
-            to your needs. Our intuitive platform streamlines the job hunt,
-            allowing you to focus on what truly matters – finding the best job
-            possible.
-          </p>
-          <p>
-            How do we do it? Through innovative algorithms and personalized
-            matching systems, we analyze your skills, preferences, and career
-            objectives to present you with tailored job recommendations. Say
-            goodbye to endless scrolling through irrelevant listings. With Know
-            Your Worth, every job suggestion is a potential opportunity waiting
-            to be seized.
-          </p>
-          <p>
-            Why choose Know Your Worth? Because we believe that everyone
-            deserves to find fulfillment in their career journey. Whether you're
-            seeking a challenging role in tech, a creative position in design,
-            or a rewarding opportunity in healthcare, we've got you covered.
-            Your worth is invaluable, and we're here to help you realize it.
+            How do we do it? We use analyze salary data from other people that
+            match your education level and work experience to help you evaluate
+            how much you are worth as an employee.
           </p>
           <p>
             Join the Know Your Worth community today and embark on a journey
@@ -84,54 +60,66 @@ export default function AppAboutUs() {
         <h1>Our Team</h1>
         <div id="team-container">
           <div className="team-member">
-            <img src="images/team-member-1.jpg" alt="Logan" loading="lazy" />
-            <h2>Logan</h2>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-              scelerisque vitae nunc ut dictum. Suspendisse potenti. Vestibulum.
-            </p>
+            <img src={img10} alt="Logan" loading="lazy" />
+            <div>
+              <h2>Logan</h2>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+                scelerisque vitae nunc ut dictum. Suspendisse potenti. Vestibulum.
+              </p>
+            </div>
           </div>
           <div className="team-member">
             <img
-              src="images/team-member-2.jpg"
+              src={img11}
               alt="Devun Durst"
               loading="lazy"
             />
-            <h2>Devun Durst</h2>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-              scelerisque vitae nunc ut dictum. Suspendisse potenti. Vestibulum.
-            </p>
+            <div>
+              <h2>Devun Durst</h2>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+                scelerisque vitae nunc ut dictum. Suspendisse potenti. Vestibulum.
+              </p>
+            </div>
           </div>
           <div className="team-member">
             <img
-              src="images/team-member-3.jpg"
+              src={img14}
               alt="Sean Walker"
               loading="lazy"
             />
-            <h2>Sean Walker</h2>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-              scelerisque vitae nunc ut dictum. Suspendisse potenti. Vestibulum.
-            </p>
+            <div>              
+              <h2>Sean Walker</h2>
+              <p>
+              I led the migration of our app to React, elevating its functionality 
+              and user experience. I also contributed to design and styling, 
+              debugged machine learning models, and managed database deployment
+              and API optimization.
+              </p>
+            </div>
           </div>
         </div>
         <div id="team-container-1">
           <div className="team-member-1">
-            <img src="images/team-member-4.jpg" alt="Hunter" loading="lazy" />
-            <h2>Hunter</h2>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-              scelerisque vitae nunc ut dictum. Suspendisse potenti. Vestibulum.
-            </p>
+            <img src={img13} alt="Hunter" loading="lazy" />
+            <div> 
+              <h2>Hunter</h2>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+                scelerisque vitae nunc ut dictum. Suspendisse potenti. Vestibulum.
+              </p>
+            </div>
           </div>
           <div className="team-member-2">
-            <img src="images/team-member-5.jpg" alt="Lincoln" loading="lazy" />
-            <h2>Lincoln</h2>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-              scelerisque vitae nunc ut dictum. Suspendisse potenti. Vestibulum.
-            </p>
+            <img src={img12} alt="Lincoln" loading="lazy" />
+            <div>
+              <h2>Lincoln</h2>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+                scelerisque vitae nunc ut dictum. Suspendisse potenti. Vestibulum.
+              </p>
+            </div>
           </div>
         </div>
       </div>
